6be3b76c2d8dab4601190e63f8d6d152f71f9a4f
shmily-han <1957618975@qq.com>
2020-02-28 16:27:04 +0800

    上传打包后的文件

---
+++ docs/precache-manifest.180a24bb8ef763e93942e13f11105474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0318120bfeb5508b54e17c25d4381350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39d728a486fc4dff3a69",
+    "url": "/static/css/2.f2bcb986.chunk.css"
+  },
+  {
+    "revision": "21e5c5121937ac69f47f",
+    "url": "/static/css/main.7fc108cf.chunk.css"
+  },
+  {
+    "revision": "39d728a486fc4dff3a69",
+    "url": "/static/js/2.3b5aac46.chunk.js"
+  },
+  {
+    "revision": "7cf0358f99ce7d99ea6e0597872ed4b0",
+    "url": "/static/js/2.3b5aac46.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21e5c5121937ac69f47f",
+    "url": "/static/js/main.fdd9be09.chunk.js"
+  },
+  {
+    "revision": "177d52efa2c7db582a05",
+    "url": "/static/js/runtime-main.5b50bb1e.js"
+  }
+]);
